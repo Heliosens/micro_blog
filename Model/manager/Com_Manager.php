ea57d8bf0e87b1b7316c039482bcef6969a9e340
Sylvie-B <unsolo@hotmail.fr>
2022-03-02 15:52:22 +0100

    comment manager

--- Model/manager/Com_Manager.php
+++ Model/manager/Com_Manager.php
@@ -12,15 +12,18 @@ class com_Manager
         $comments = [];
         $query = DB::getPDO()->query("SELECT * FROM comment");
         if($query){
-            $com_Manager = new com_Manager();
+            $userManager = new UserManager();
+            $artManager = new Article_Manager();
 
             foreach ($query->fetchAll() as $comData){
                 $comments = (new Comment())
-                    ->setArticle($com_Manager->getArtById($comData['article']))
-                    ->setContent($comData['content'])
                     ->setId($comData['id'])
-                    ;
+                    ->setAuthor($userManager->getUserById($comData['user_fk']))
+                    ->setContent($comData['content'])
+                    ->setArticle($artManager->getArticleById($comData['article_fk']))
+                ;
             }
         }
+        return $comments;
     }
 }
