38240b76d98570df8736bc63a249da29f6b92b7f
Sylvie-B <unsolo@hotmail.fr>
2022-03-02 15:28:07 +0100

    role manager

--- Model/manager/Role_manager.php
+++ Model/manager/Role_manager.php
@@ -2,7 +2,23 @@
 
 namespace App\Model\manager;
 
+use App\Model\DB;
+use App\Model\entity\Role;
+
 class role_manager
 {
-
+    public function findAllRole () : array
+    {
+        $roles = [];
+        $query = DB::getPDO()->query("SELECT * FROM role");
+        if($query){
+            foreach ($query->fetchAll() as $roleData){
+                $roles[] = (new Role())
+                    ->setId($roleData['id'])
+                    ->setRoleName($roleData['role_name'])
+                    ;
+            }
+        }
+        return $roles;
+    }
 }
