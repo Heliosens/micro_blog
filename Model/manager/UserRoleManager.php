285e7ba8770f4fcdbd19d29243b6ec10c21c082a
Sylvie-B <unsolo@hotmail.fr>
2022-03-02 16:34:25 +0100

    user role manager

--- Model/manager/UserRoleManager.php
+++ Model/manager/UserRoleManager.php
@@ -2,7 +2,28 @@
 
 namespace App\Model\manager;
 
+use App\Model\DB;
+use App\Model\entity\User;
+
 class UserRoleManager
 {
-
+    public function getUserByRoleId ($roleId) :array {
+        $users = [];
+        $query = DB::getPDO()->query("
+            SELECT * FROM user WHERE id IN (SELECT user_fk FROM user_role WHERE role_fk = $roleId)
+            ");
+        if($query){
+            foreach ($query->fetchAll() as $user){
+                $users = (new User())
+                    ->setId($user['id'])
+                    ->setMail($user['email'])
+                    ->setFirstname($user['firstname'])
+                    ->setLastname($user['lastname'])
+                    ->setPassword($user['password'])
+                    ->setAge($user['age'])
+                    ;
+            }
+        }
+        return $users;
+    }
 }
