ed6777d195376d8463ddb22d1e0896a74a0cdd81
Sylvie-B <unsolo@hotmail.fr>
2022-03-03 14:01:23 +0100

    requete com by art id

--- Model/manager/Com_Manager.php
+++ Model/manager/Com_Manager.php
@@ -7,23 +7,23 @@ use App\Model\entity\Comment;
 
 class Com_Manager
 {
-    public function comByArtId () : array
+    public function comByArtId ($idArt) : array
     {
-//        $comments = [];
-//        $query = DB::getPDO()->query("SELECT * FROM comment");
-//        if($query){
-//            $userManager = new UserManager();
-//            $artManager = new Article_Manager();
-//
-//            foreach ($query->fetchAll() as $comData){
-//                $comments[] = (new Comment())
-//                    ->setId($comData['id'])
-//                    ->setAuthor($userManager->getUserById($comData['user_fk']))
-//                    ->setContent($comData['content'])
-//                    ->setArticle($artManager->getArticleById($comData['article_fk']))
-//                ;
-//            }
-//        }
-//        return $comments;
+        $comments = [];
+        $query = DB::getPDO()->query("
+            SELECT * FROM comment WHERE article_fk IN (SELECT article.id FROM article WHERE article.id = $idArt)");
+        if($query){
+            $userManager = new UserManager();
+            $artManager = new Article_Manager();
+            foreach ($query->fetchAll() as $comData){
+                $comments[] = (new Comment())
+                    ->setId($comData['id'])
+                    ->setAuthor($userManager->getUserById($comData['user_fk']))
+                    ->setContent($comData['content'])
+                    ->setArticle($artManager->getArticleById($comData['article_fk']))
+                ;
+            }
+        }
+        return $comments;
     }
 }
