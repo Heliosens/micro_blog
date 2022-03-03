15ed15a0a24501de6083e84e28412fd053d99f74
Sylvie-B <unsolo@hotmail.fr>
2022-03-03 11:38:14 +0100

    find All user

--- Model/manager/UserManager.php
+++ Model/manager/UserManager.php
@@ -7,6 +7,26 @@ use App\Model\entity\User;
 
 class UserManager
 {
+
+    public function findAll(): array
+    {
+        $query = DB::getPDO()->query("SELECT * FROM user");
+        $users = [];
+        if($query){
+            foreach ($query->fetchAll() as $data){
+                $users[] = (new User())
+                    ->setId($data['id'])
+                    ->setMail($data['email'])
+                    ->setAge($data['age'])
+                    ->setPassword($data['password'])
+                    ->setLastname($data['lastname'])
+                    ->setFirstname($data['firstname'])
+                ;
+            }
+        }
+        return $users;
+    }
+
     public function getUserById ($id) :User {
         $query = DB::getPDO()->query("SELECT * FROM user WHERE id = $id");
         $user = new User();
