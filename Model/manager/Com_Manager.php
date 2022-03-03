186ba7656de845f951b323a3b5dc30a5255108ca
Sylvie-B <unsolo@hotmail.fr>
2022-03-03 11:37:48 +0100

    suppr all com

--- Model/manager/Com_Manager.php
+++ Model/manager/Com_Manager.php
@@ -7,23 +7,23 @@ use App\Model\entity\Comment;
 
 class Com_Manager
 {
-    public function findAllCom () : array
+    public function comByArtId () : array
     {
-        $comments = [];
-        $query = DB::getPDO()->query("SELECT * FROM comment");
-        if($query){
-            $userManager = new UserManager();
-            $artManager = new Article_Manager();
-
-            foreach ($query->fetchAll() as $comData){
-                $comments[] = (new Comment())
-                    ->setId($comData['id'])
-                    ->setAuthor($userManager->getUserById($comData['user_fk']))
-                    ->setContent($comData['content'])
-                    ->setArticle($artManager->getArticleById($comData['article_fk']))
-                ;
-            }
-        }
-        return $comments;
+//        $comments = [];
+//        $query = DB::getPDO()->query("SELECT * FROM comment");
+//        if($query){
+//            $userManager = new UserManager();
+//            $artManager = new Article_Manager();
+//
+//            foreach ($query->fetchAll() as $comData){
+//                $comments[] = (new Comment())
+//                    ->setId($comData['id'])
+//                    ->setAuthor($userManager->getUserById($comData['user_fk']))
+//                    ->setContent($comData['content'])
+//                    ->setArticle($artManager->getArticleById($comData['article_fk']))
+//                ;
+//            }
+//        }
+//        return $comments;
     }
 }
